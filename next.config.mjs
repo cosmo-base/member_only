f9467746c947b6f111cb5d526ce86dda819849b1
cosmo-base <cosmobase.official@gmail.com>
2026-03-30 15:21:13 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
-}
 
-export default nextConfig
+  basePath: '/member_only', 
+  assetPrefix: '/member_only', 
+};
+
+export default nextConfig;
