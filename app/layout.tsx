9b9d8b6cad2a64e4a759dce37a27c23636de51e7
cosmo-base <cosmobase.official@gmail.com>
2026-03-30 15:57:27 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,15 +12,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/CB_icon.png',
+        url: '/member-only/CB_icon.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/CB_icon.png',
+        url: '/member-only/CB_icon.png',
         media: '(prefers-color-scheme: dark)',
       },
     ],
-    apple: '/CB_icon.png',
+    apple: '/member-only/CB_icon.png',
   },
 }
 
