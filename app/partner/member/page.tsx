fb0bd57a1d12ac249fc7984ad817a29851c4951c
cosmo-base <cosmobase.official@gmail.com>
2026-06-08 12:09:58 +0900

    Update page.tsx

--- app/partner/member/page.tsx
+++ app/partner/member/page.tsx
@@ -2,6 +2,7 @@
 "use client"
 
 import { useState } from "react"
+import { StarBackground } from "@/components/star-background"
 import { ContentPageLayout } from "@/components/content-page-layout"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
