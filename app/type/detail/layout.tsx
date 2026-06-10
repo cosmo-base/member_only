ae7eec7de0a7a5c2a6edc49dea8231ba70be97cd
cosmo-base <cosmobase.official@gmail.com>
2026-06-10 11:17:57 +0900

    Update layout.tsx

--- app/type/detail/layout.tsx
+++ app/type/detail/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 
 export const metadata: Metadata = {
   title: "宇宙タイプ診断 完全版 | Cosmo Base参加者ページ",
+  description: "24問の診断で、あなたが宇宙で活躍する未来がわかる。宇宙診断コンテンツ。",
 }
 
 export default function SubPageLayout({
